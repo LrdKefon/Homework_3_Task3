9c17e6f97364d7f16a8d9e1343894d6160cc1a42
Alex <zazerny@gmail.com>
2023-02-11 22:53:30 +0300

    Task done

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         CreditPaymentService service = new CreditPaymentService();
-        int summa = 1_000_000;
+        int summa = 1_000_000;// 2 варианта, новая переменная и с внесением в метод.
         int period = 12;
-        int paymentOneMonth = service.calculate(summa, period);
+        int paymentOneMonth = service.calculate(summa, period); // новая переменная
         System.out.println();
         System.out.println("Сумма месячного платежа: " + paymentOneMonth);
+        System.out.println("Процентная ставка: 9,99%");
 
         System.out.println();
         System.out.println("Сумма месячного платежа: "
                 + service.calculate(1_000_000, 24));
+        System.out.println("Процентная ставка: 9,99%");
 
         System.out.println();
         System.out.println("Сумма месячного платежа: "
                 + service.calculate(1_000_000, 36));
+        System.out.println("Процентная ставка: 9,99%");
 
 
     }
